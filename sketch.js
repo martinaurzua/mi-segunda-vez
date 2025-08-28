437ec7c5c10154e2c44fa3c1d080a5cffb596711
Martina Urzúa <martina.urzua@ug.uchile.cl>
2025-08-27 22:51:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,15 +13,16 @@ function setup() {
 function draw() {
     background(216, 191, 216);
     orbitControl();
-    pointLight(255, 255, 255, valor / 3, valor / 2, 50);
-    specularMaterial(255);
+    directionalLight(200, 200, 200, -1, -1, -1);
+    pointLight(255, 255, 255, 200, -150, 200);
+    specularMaterial(200, 200, 255);
     shininess(50);
+    emissiveMaterial(50, 50, 100);
     metalness(1);
     fill(139, 7, 139);
-    ellipsoid(30);
+    sphere(40, 64, 64);
 }
 
 function windowResized() {
     resizeCanvas(valor, valor);
 }
-
